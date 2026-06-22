f61f7fb2f54a911b9bfffd7caad0929bf7da4559
Fazal Abbas <fazalabbas2002@gmail.com>
2026-06-22 16:39:31 +0500

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,6 @@ async function connectDB() {
 // Start server
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, async () => {
-  await connectDB();
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
